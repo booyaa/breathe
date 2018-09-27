4fe24940d4aa22d4c92020711c5687f992ac3bb9
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2018-09-27 19:16:40 +0100

    adding knobs to Printer component

--- src/Components/Printer/stories.js
+++ src/Components/Printer/stories.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, object } from '@storybook/addon-knobs';
 
 import Printer from '.';
 import Child from '../Child';
 
 storiesOf('Printer', module)
-.add('default', () => <Printer />)
+.addDecorator(withKnobs)
+.add('default', () => <Printer value={object('value')}/>)
 .add('string', () => <Printer value='i am a string'/>)
 .add('boolean', () => <Printer value={true}/>)
 .add('children prop', () => <Printer><Child /></Printer>);
